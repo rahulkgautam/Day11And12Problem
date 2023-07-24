63d3e80a37bd2b377be2d80c5bc4b41cf6c92514
rahulkgautam <rahulkgautam125@gmail.com>
2023-07-25 00:12:06 +0530

    Count Frequency in Array

--- Program.cs
+++ Program.cs
@@ -7,28 +7,7 @@ namespace Day11And12Problem
     {
         static void Main(string[] args)
         {
-            int[] arr = { 1, 2, 3, 4, 2, 1, 3, 2, 4, 4, 5, 1 };
-            Dictionary<int, int> frequencyMap = new Dictionary<int, int>();
-
-            // Count the frequency of each element
-            foreach (int element in arr)
-            {
-                if (frequencyMap.ContainsKey(element))
-                {
-                    frequencyMap[element]++;
-                }
-                else
-                {
-                    frequencyMap[element] = 1;
-                }
-            }
-
-            // Display the result
-            Console.WriteLine("Element\tFrequency");
-            foreach (var pair in frequencyMap)
-            {
-                Console.WriteLine($"{pair.Key}\t{pair.Value}");
-            }
+            CountFrequency.Frequency();
         }
     }
     
