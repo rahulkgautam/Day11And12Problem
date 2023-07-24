3616da042ad18b9dbecec6e56f5896280c3622a9
rahulkgautam <rahulkgautam125@gmail.com>
2023-07-24 23:41:28 +0530

    Welcome to pattern program and logical

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day11And12Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to pattern program and logical");
+        }
+    }
+    
+}
