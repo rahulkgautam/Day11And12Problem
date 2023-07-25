cf93c056c131afb00f29105e866128754bc56ea3
rahulkgautam <rahulkgautam125@gmail.com>
2023-07-25 22:07:27 +0530

    Print start 7*7 rectangle pattern program

--- PatternProgram.cs
+++ PatternProgram.cs
@@ -8,14 +8,24 @@ namespace Day11And12Problem
     {
         public static void Pattern()
         {
-            for (int i = 8; i >= 1; i--)
+            int length = 7, width = 7;
+            Console.Write(" ");
+
+            for (int i = 1; i <= width; i++)
             {
-                for (int j = 1; j <= i; ++j)
+                for (int j = 1; j <= length; j++)
                 {
-                    Console.Write("*");
+                    if (i == 1 || i == width || j == 1 || j == length)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
                 }
-                Console.WriteLine();
+                Console.Write("\n ");
             }
-        }
+         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Day11And12Problem
     {
         static void Main(string[] args)
         {
+
             PatternProgram.Pattern();
         }
     }
